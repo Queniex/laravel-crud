cd2ff4893b35ed11761bb7d94b7fffcb1d862d15
Queniex <Queniesalbiyah@gmail.com>
2023-08-25 22:28:52 +0700

    integration laravel and vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
+
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component('dashboard', require('./components/Dashboard.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
