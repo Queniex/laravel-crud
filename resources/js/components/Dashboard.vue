a2cbe9efddb8eb8a10f14df896748be379606fb9
Queniex <Queniesalbiyah@gmail.com>
2023-08-25 22:29:03 +0700

    Add dashboard page

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="bg-white py-1 px-1">
+                <div>
+                    Hello, I'm Quenie!
+                    <div>
+                        I would like to make a simple CRUD project using Laravel
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
+
+<style>
+/* * {
+    border: 1px solid red;
+} */
+</style>
